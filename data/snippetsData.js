6d15cf1ada1928a1f68f81bbc78dd5405a3314da
anuuyu <yusrandante@gmail.com>
2022-05-14 07:24:12 +0800

    add layout to display snippets data list

---
+++ data/snippetsData.js
@@ -0,0 +1,28 @@
+const snippetsData = [
+  {
+    title: 'Snippet',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/snippets/snippet',
+  },
+  {
+    title: 'Snippet',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/snippets/snippet1',
+  },
+  {
+    title: 'Snippet',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/snippets/snippet2',
+  },
+  {
+    title: 'Snippet',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/snippets/snippet3',
+  },
+]
+
+export default snippetsData
