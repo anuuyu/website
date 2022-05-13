79f94c6615d09c965d7cbe967fc6eed9398d08cc
anuuyu <yusrandante@gmail.com>
2022-05-13 22:03:42 +0800

    add uses link

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
+  { href: '/uses', title: 'Uses' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
