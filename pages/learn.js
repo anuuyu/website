258b37fdbc649412c998f61e177787a7a0466883
anuuyu <yusrandante@gmail.com>
2022-05-14 09:51:55 +0800

    run linter

--- pages/learn.js
+++ pages/learn.js
@@ -13,7 +13,9 @@ export default function Learn() {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Learn
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">referensi belajar web developer gratis</p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            referensi belajar web developer gratis
+          </p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
@@ -29,7 +31,9 @@ export default function Learn() {
           </div>
         </div>
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">referensi belajar berbayar</p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            referensi belajar berbayar
+          </p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
