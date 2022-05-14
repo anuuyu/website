9210ad3dc64ce4545c3033dcb28862f970e074e3
anuuyu <yusrandante@gmail.com>
2022-05-14 08:29:28 +0800

    remove tags link from navbar

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,5 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
   { href: '/uses', title: 'Uses' },
   { href: '/snippets', title: 'Snippets' },
   { href: '/projects', title: 'Projects' },
