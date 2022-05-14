9a3938fafaa468e5057cfd6e59ede313842a54c7
anuuyu <yusrandante@gmail.com>
2022-05-15 07:10:09 +0800

    rename snippets > notes

--- data/notesData.js
+++ data/notesData.js
@@ -1,28 +1,28 @@
-const snippetsData = [
+const notesData = [
   {
     title: 'Snippet',
     description: `daftar open source software yang dibuat orang indonesia.`,
     imgSrc: '/static/images/ossmii.png',
-    href: '/snippets/snippet',
+    href: '/notes/snippet',
   },
   {
     title: 'Snippet',
     description: `daftar open source software yang dibuat orang indonesia.`,
     imgSrc: '/static/images/ossmii.png',
-    href: '/snippets/snippet1',
+    href: '/notes/snippet1',
   },
   {
     title: 'Snippet',
     description: `daftar open source software yang dibuat orang indonesia.`,
     imgSrc: '/static/images/ossmii.png',
-    href: '/snippets/snippet2',
+    href: '/notes/snippet2',
   },
   {
     title: 'Snippet',
     description: `daftar open source software yang dibuat orang indonesia.`,
     imgSrc: '/static/images/ossmii.png',
-    href: '/snippets/snippet3',
+    href: '/notes/snippet3',
   },
 ]
 
-export default snippetsData
+export default notesData
