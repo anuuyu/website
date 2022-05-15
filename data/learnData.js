1e72c30c68efc3bbdc3c9bf511877794e94be4dd
anuuyu <yusrandante@gmail.com>
2022-05-15 18:32:46 +0800

    update link

--- data/learnData.js
+++ data/learnData.js
@@ -3,25 +3,25 @@ const learnData = [
     title: 'Snippet',
     description: `daftar open source software yang dibuat orang indonesia.`,
     imgSrc: '/static/images/ossmii.png',
-    href: '/snippets/snippet',
+    href: '/notes/snippet',
   },
   {
     title: 'Snippet',
     description: `daftar open source software yang dibuat orang indonesia.`,
     imgSrc: '/static/images/ossmii.png',
-    href: '/snippets/snippet1',
+    href: '/notes/snippet1',
   },
   {
     title: 'Snippet',
     description: `daftar open source software yang dibuat orang indonesia.`,
     imgSrc: '/static/images/ossmii.png',
-    href: '/snippets/snippet2',
+    href: '/notes/snippet2',
   },
   {
     title: 'Snippet',
     description: `daftar open source software yang dibuat orang indonesia.`,
     imgSrc: '/static/images/ossmii.png',
-    href: '/snippets/snippet3',
+    href: '/notes/snippet3',
   },
 ]
 
