ad320c1c85bb20fc5855eaba13f6aa466ddde0ae
anuuyu <yusrandante@gmail.com>
2022-05-19 19:19:54 +0800

    menambah laman tools and resources

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/uses', title: 'Uses' },
   { href: '/notes', title: 'Notes' },
+  { href: '/resources', title: 'Tools' },
   { href: '/projects', title: 'Projects' },
   { href: '/learn', title: 'Learn' },
   { href: '/about', title: 'About' },
