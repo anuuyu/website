54c2a3afadbad9091fe286e5d57db5a9dd9dca48
anuuyu <yusrandante@gmail.com>
2022-05-22 07:44:39 +0800

    update learn

--- data/learnData.js
+++ data/learnData.js
@@ -5,24 +5,6 @@ const learnData = [
     imgSrc: '/static/images/ossmii.png',
     href: '/notes/snippet',
   },
-  {
-    title: 'Snippet',
-    description: `daftar open source software yang dibuat orang indonesia.`,
-    imgSrc: '/static/images/ossmii.png',
-    href: '/notes/snippet1',
-  },
-  {
-    title: 'Snippet',
-    description: `daftar open source software yang dibuat orang indonesia.`,
-    imgSrc: '/static/images/ossmii.png',
-    href: '/notes/snippet2',
-  },
-  {
-    title: 'Snippet',
-    description: `daftar open source software yang dibuat orang indonesia.`,
-    imgSrc: '/static/images/ossmii.png',
-    href: '/notes/snippet3',
-  },
 ]
 
 export default learnData
