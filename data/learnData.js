89592e8e18fa921c1d6e3edc0fc9c09b61a4e276
anuuyu <yusrandante@gmail.com>
2022-05-26 20:44:48 +0800

    hapus courseData dan gabung ke learnData. learnData  > free dan premium

--- data/learnData.js
+++ data/learnData.js
@@ -1,4 +1,4 @@
-const learnData = [
+export const freeData = [
   {
     title: 'Snippet',
     description: `daftar open source software yang dibuat orang indonesia.`,
@@ -7,4 +7,23 @@ const learnData = [
   },
 ]
 
-export default learnData
+export const premiumData = [
+  {
+    title: 'Three.js Journey',
+    description: `The Best Way to Learn Three.js.`,
+    imgSrc: '/static/images/page/threejs-journey.svg',
+    href: 'https://threejs-journey.com/',
+  },
+  {
+    title: 'Epic React',
+    description: `Get Really Good at React.`,
+    imgSrc: '/static/images/page/epicreact.svg',
+    href: 'https://epicreact.dev/',
+  },
+  {
+    title: 'Testing JavaScript',
+    description: `Learn the smart, efficient way to test any JavaScript application.`,
+    imgSrc: '/static/images/page/epicreact.svg',
+    href: 'https://testingjavascript.com/',
+  },
+]
