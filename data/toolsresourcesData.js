80d862ec40451cc2234dc06e5172fa3262b74888
anuuyu <yusrandante@gmail.com>
2022-05-26 20:34:52 +0800

    gabung toolsData dan resourcesData menjadi toolsresourcesData

---
+++ data/toolsresourcesData.js
@@ -0,0 +1,53 @@
+export const toolsData = [
+  {
+    title: 'Tools',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/notes/snippet',
+  },
+  {
+    title: 'Tools',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/notes/snippet1',
+  },
+  {
+    title: 'Tools',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/notes/snippet2',
+  },
+  {
+    title: 'Tools',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/notes/snippet3',
+  },
+]
+
+export const resourcesData = [
+  {
+    title: 'Resources',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/notes/snippet',
+  },
+  {
+    title: 'Resources',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/notes/snippet1',
+  },
+  {
+    title: 'Resources',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/notes/snippet2',
+  },
+  {
+    title: 'Resources',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '/notes/snippet3',
+  },
+]
