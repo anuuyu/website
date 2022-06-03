ef3043c6b1fb74fd5a747112b74b389dcf9efe5d
anuuyu <yusrandante@gmail.com>
2022-06-03 17:44:34 +0800

    challenges page

---
+++ data/challengesData.js
@@ -0,0 +1,22 @@
+const challengesData = [
+  {
+    title: 'Codier',
+    description: `Front-end coding challenges.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: 'https://codier.io/',
+  },
+  {
+    title: 'Frontend Mentor',
+    description: `Frontend Mentor | Front-end coding challenges using a real-life workflow.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: 'https://www.frontendmentor.io/',
+  },
+  {
+    title: 'devChallenges.io',
+    description: `devChallenges.io | Real-life Coding Challenges to become a Software Engineer.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: 'https://devchallenges.io/',
+  },
+]
+
+export default challengesData
---
+++ pages/challenges.js
@@ -0,0 +1,38 @@
+import siteMetadata from '@/data/siteMetadata'
+import challengesData from '@/data/challengesData'
+import Card from '@/components/Card'
+import { PageSEO } from '@/components/SEO'
+
+export default function Challenges() {
+  return (
+    <>
+      <PageSEO
+        title={`Challenges - ${siteMetadata.author}`}
+        description={siteMetadata.description}
+      />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Challenges
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Daftar tantangan yang pernah dan sedang kujalankan berhubungan dengan web development.
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {challengesData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
