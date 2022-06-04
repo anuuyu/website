5bc02c7f0701d3a19a8071593340020f78ee6f06
anuuyu <yusrandante@gmail.com>
2022-06-04 22:35:15 +0800

    add bookmarks page

---
+++ data/bookmarksData.js
@@ -0,0 +1,16 @@
+const bookmarksData = [
+  {
+    title: 'Made in Indonesia',
+    description: `daftar open source software yang dibuat orang indonesia.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: 'https://github.com/anuuyu/made-in-indonesia',
+  },
+  {
+    title: 'NextJs Links Starter',
+    description: `NextJs templates to create links list.`,
+    imgSrc: '/static/images/ossmii.png',
+    href: '#',
+  },
+]
+
+export default bookmarksData
