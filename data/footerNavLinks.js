7e71cf21ef192bbfff3f8030182c6129285f3b8b
anuuyu <yusrandante@gmail.com>
2022-06-18 13:23:39 +0800

    remove headerNavLinks and footerNavLinks

--- data/footerNavLinks.js
+++
@@ -1,9 +0,0 @@
-const footerNavLinks = [
-  { href: '/bookmarks', title: 'Bookmarks' },
-  { href: '/books', title: 'Books' },
-  { href: '/challenges', title: 'Challenges' },
-  { href: '/links', title: 'Links' },
-  { href: '/wishlist', title: 'Wishlist' },
-]
-
-export default footerNavLinks
