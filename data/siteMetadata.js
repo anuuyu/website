19ec45d7ca07ad9d752ecfba96d123901379e8e6
anuuyu <yusrandante@gmail.com>
2022-06-27 20:42:52 +0800

    :smile: remove unused analytic tools

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,14 +17,9 @@ const siteMetadata = {
   devto: 'https://dev.to/yu',
   locale: 'en-US',
   analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
+    // add app.posthog.com to the content security policy in the `next.config.js` file.
+    // PostHog Analytics
     posthogAnalytics: true, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
