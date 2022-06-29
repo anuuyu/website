270a7c03346476b438eaa61763f7b91e0f6da606
anuuyu <yusrandante@gmail.com>
2022-06-29 22:48:23 +0800

    add scroll to top button bookmarks page

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -2,6 +2,7 @@ import siteMetadata from '@/data/siteMetadata'
 import bookmarksData from '@/data/bookmarksData'
 import { BookmarksCard } from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
+import ScrollTop from '@/components/ScrollTop'
 
 export default function Bookmarks() {
   return (
@@ -10,6 +11,7 @@ export default function Bookmarks() {
         title={`Bookmarks - ${siteMetadata.author}`}
         description={siteMetadata.description}
       />
+      <ScrollTop />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
