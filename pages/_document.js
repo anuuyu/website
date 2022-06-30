43d41bee4730ca307e54a670ec2b03947b4dba92
anuuyu <yusrandante@gmail.com>
2022-06-30 09:13:44 +0800

    update theme-color

--- pages/_document.js
+++ pages/_document.js
@@ -19,12 +19,12 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="theme-color" media="prefers-color-scheme: dark" content="#000000" />{' '}
-          <meta name="theme-color" media="prefers-color-scheme: light" content="#ffffff" />
+          <meta name="msapplication-TileColor" content="#e7ede7" />
+          <meta name="theme-color" media="prefers-color-scheme: dark" content="#e7ede7" />{' '}
+          <meta name="theme-color" media="prefers-color-scheme: light" content="#030f03" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         </Head>
-        <body className="bg-white text-black antialiased dark:bg-[#030f03] dark:text-white">
+        <body className="bg-[#e7ede7] text-black antialiased dark:bg-[#030f03] dark:text-white">
           <Main />
           <NextScript />
         </body>
