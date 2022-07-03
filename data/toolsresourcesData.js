7e70acce344cce933201de56abc874606c255261
anuuyu <yusrandante@gmail.com>
2022-07-04 07:15:34 +0800

    update

--- data/toolsresourcesData.js
+++ data/toolsresourcesData.js
@@ -440,4 +440,10 @@ export const premiumLearningResourcesData = [
     imgSrc: 'learnjavascripttoday.svg',
     href: 'https://learnjavascript.today/',
   },
+  {
+    title: 'DesignCode io',
+    description: `Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.`,
+    imgSrc: 'designcodeio.svg',
+    href: 'https://designcode.io/',
+  },
 ]
