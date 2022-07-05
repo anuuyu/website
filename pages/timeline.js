bd1e2ed50d062977118d23807fcb918e79af9502
anuuyu <yusrandante@gmail.com>
2022-07-05 20:41:22 +0800

    update timeline

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,6 +1,4 @@
 import siteMetadata from '@/data/siteMetadata'
-import timelineData from '@/data/timelineData'
-import { Card } from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
 
 export default function Timeline() {
@@ -12,20 +10,127 @@ export default function Timeline() {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Timeline
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">project list</p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            My timeline Discover and Explore new things.
+          </p>
         </div>
         <div className="container py-12">
-          <div className="-m-4 flex flex-wrap">
-            {timelineData.map((d) => (
-              <Card
-                key={d.title}
-                title={d.title}
-                description={d.description}
-                imgSrc={d.imgSrc}
-                href={d.href}
-              />
-            ))}
-          </div>
+          <ol className="relative border-l border-gray-200 dark:border-gray-700">
+            <li className="mb-10 ml-4">
+              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                July 2022
+              </time>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Explore Twind</h3>
+              <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                The smallest, fastest, most feature complete tailwind-in-js solution in existence
+              </p>
+              <a
+                href="https://twind.dev/"
+                className="inline-flex items-center rounded-lg border border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+              >
+                Learn more{' '}
+                <svg
+                  className="ml-2 h-3 w-3"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </li>
+            <li className="mb-10 ml-4">
+              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                July 2022
+              </time>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                Explore Deno Deploy
+              </h3>
+              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                Deploy JavaScript Globally.
+              </p>
+              <a
+                href="https://deno.com/deploy"
+                className="inline-flex items-center rounded-lg border border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+              >
+                Learn more{' '}
+                <svg
+                  className="ml-2 h-3 w-3"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </li>
+            <li className="mb-10 ml-4">
+              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                July 2022
+              </time>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                Explore fresh - The next-gen web framework.
+              </h3>
+              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                Fresh is a next generation web framework, built for speed, reliability, and
+                simplicity.
+              </p>
+              <a
+                href="https://fresh.deno.dev/"
+                className="inline-flex items-center rounded-lg border border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+              >
+                Learn more{' '}
+                <svg
+                  className="ml-2 h-3 w-3"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </li>
+            <li className="mb-10 ml-4">
+              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                2010 - 2020
+              </time>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                Undocumented story.
+              </h3>
+              <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                Learn a lot of things, including HTML, CSS, JavaScript, XML, Blogspot, PHP,
+                wordpress.
+              </p>
+            </li>
+            <li className="ml-4">
+              <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                2010
+              </time>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                Learn HTML and CSS
+              </h3>
+              <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                my enter door to web development.
+              </p>
+            </li>
+          </ol>
         </div>
       </div>
     </>
