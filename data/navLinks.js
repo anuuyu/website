bbdd9fdf0144e919cd6ec74096dcb5c24b9b0237
anuuyu <yusrandante@gmail.com>
2022-07-21 15:14:49 +0800

    update navlinks

--- data/navLinks.js
+++ data/navLinks.js
@@ -2,16 +2,12 @@ export const headerNavLinks = [
   { href: '/posts', title: 'Posts' },
   { href: '/uses', title: 'Uses' },
   { href: '/notes', title: 'Notes' },
-  { href: '/resources', title: 'Tools' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
 export const footerNavLinks = [
   { href: '/bookmarks', title: 'Bookmarks' },
-  { href: '/books', title: 'Books' },
-  { href: '/challenges', title: 'Challenges' },
-  { href: '/links', title: 'Links' },
   { href: '/timeline', title: 'Timeline' },
   { href: '/wishlist', title: 'Wishlist' },
 ]
