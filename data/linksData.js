24b30ae11255cd44f82d15786c0780a217546ea3
anuuyu <yusrandante@gmail.com>
2022-07-21 15:10:16 +0800

    remove booksdata, challengesdata, linksdata, and toolsresourcesdata after move into bookmarks data

--- data/linksData.js
+++
@@ -1,8 +0,0 @@
-export const twlinksData = [
-  {
-    title: 'Andy Crestodina (@crestodina)',
-    description: `Speaker, content marketer, co-founder of Orbit Media (@orbiteers) and author of Content Chemistry.`,
-    imgSrc: '/static/images/page/thumbnail.svg',
-    href: 'https://twitter.com/crestodina',
-  },
-]
