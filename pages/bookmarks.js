b7817a30d0edcda75db782484547fd104a524b11
anuuyu <yusrandante@gmail.com>
2022-07-21 15:00:20 +0800

    move books, challenges, tools, resources, learning resources and link data into bookmarks

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -1,6 +1,15 @@
 import siteMetadata from '@/data/siteMetadata'
-import bookmarksData from '@/data/bookmarksData'
-import { BookmarksCard } from '@/components/Card'
+import {
+  bookmarksData,
+  booksData,
+  challengesData,
+  toolsData,
+  resourcesData,
+  freeLearningResourcesData,
+  premiumLearningResourcesData,
+  twlinksData,
+} from '@/data/bookmarksData'
+import { BookmarksCard, BooksCard, CardsPage, ToolsPage, Card } from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
 import ScrollTop from '@/components/ScrollTop'
 
@@ -25,6 +34,133 @@ export default function Bookmarks() {
           <div className="-m-4 flex flex-wrap">
             {bookmarksData.map((d) => (
               <BookmarksCard
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Books and e-books need to read
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {booksData.map((d) => (
+              <BooksCard
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                author={d.author}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Challenges I've been and are currently doing
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {challengesData.map((d) => (
+              <CardsPage
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h3 className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            free learning Resources
+          </h3>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {toolsData.map((d) => (
+              <ToolsPage
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            premium learning resources
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {resourcesData.map((d) => (
+              <ToolsPage
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            awesome tools around the web
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {freeLearningResourcesData.map((d) => (
+              <ToolsPage
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            awesome resources around the web
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {premiumLearningResourcesData.map((d) => (
+              <ToolsPage
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            i followed a lot awesome people on twitter, this my highlight.
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {twlinksData.map((d) => (
+              <Card
                 key={d.title}
                 title={d.title}
                 description={d.description}
