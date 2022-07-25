078f51006ae702642ad3caa3094bd6d46c04a89e
anuuyu <yusrandante@gmail.com>
2022-07-25 22:46:19 +0800

    update data

--- data/wishlistData.js
+++ data/wishlistData.js
@@ -1,15 +1,43 @@
 const wishlistData = [
   {
-    title: 'Made in Indonesia',
-    description: `daftar open source software yang dibuat orang indonesia.`,
-    imgSrc: '/static/images/page/thumbnail.svg',
-    href: 'https://github.com/anuuyu/made-in-indonesia',
+    title: 'MacBook Pro',
+    imgSrc: '/static/images/page/wishlist/mbp16.jpeg',
+    href: 'https://www.apple.com/shop/buy-mac/macbook-pro/16-inch-space-gray-10-core-cpu-16-core-gpu-512gb',
   },
   {
-    title: 'NextJs Links Starter',
-    description: `NextJs templates to create links list.`,
-    imgSrc: '/static/images/page/thumbnail.svg',
-    href: '#',
+    title: 'Mac Studio',
+    imgSrc: '/static/images/page/wishlist/mac-studio.jpeg',
+    href: 'https://www.apple.com/shop/buy-mac/mac-studio/10-core-cpu-24-core-gpu-16-core-neural-engine-32gb-memory-512gb',
+  },
+  {
+    title: 'Ipad Pro',
+    imgSrc: '/static/images/page/wishlist/ipad-pro-12.jpeg',
+    href: 'https://www.apple.com/shop/buy-ipad/ipad-pro/12.9-inch-display-256gb-space-gray-wifi',
+  },
+  {
+    title: 'iPhone 13 Pro Max',
+    imgSrc: '/static/images/page/wishlist/iphone-13-pro-max.png',
+    href: 'https://www.apple.com/shop/buy-iphone/iphone-13-pro',
+  },
+  {
+    title: 'AirPods',
+    imgSrc: '/static/images/page/wishlist/airpods.jpeg',
+    href: 'https://www.apple.com/shop/product/MME73AM/A/airpods-3rd-generation',
+  },
+  {
+    title: 'MX Master 3S',
+    imgSrc: '/static/images/page/wishlist/mx-master-3s.webp',
+    href: 'https://www.logitech.com/en-us/products/mice/mx-master-3s.910-006557.html',
+  },
+  {
+    title: 'ZSA Moonlander',
+    imgSrc: '/static/images/page/wishlist/zsa-moonlander.png',
+    href: 'https://www.zsa.io/moonlander/',
+  },
+  {
+    title: 'UDESK PRO',
+    imgSrc: '/static/images/page/wishlist/udesk.webp',
+    href: 'https://theudesk.com/',
   },
 ]
 
